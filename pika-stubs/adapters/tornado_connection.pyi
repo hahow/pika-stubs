7f391638b8545f0fcbc1a3b01ea253c191598709
Chi-En Wu <jason.wu@hahow.in>
2020-06-08 15:59:50 +0800

    refactor: refine typings for `adapters.tornado_connection`

--- pika-stubs/adapters/tornado_connection.pyi
+++ pika-stubs/adapters/tornado_connection.pyi
@@ -1,10 +1,47 @@
-from pika.adapters import base_connection as base_connection
-from pika.adapters.utils import nbio_interface as nbio_interface, selector_ioloop_adapter as selector_ioloop_adapter
-from typing import Any, Optional
+from typing import Callable, Optional, Sequence, Union
 
-LOGGER: Any
+import tornado.ioloop
 
-class TornadoConnection(base_connection.BaseConnection):
-    def __init__(self, parameters: Optional[Any] = ..., on_open_callback: Optional[Any] = ..., on_open_error_callback: Optional[Any] = ..., on_close_callback: Optional[Any] = ..., custom_ioloop: Optional[Any] = ..., internal_connection_workflow: bool = ...) -> None: ...
+from .. import connection
+from . import base_connection
+from .utils import connection_workflow, nbio_interface
+
+_OnCloseCallback = Callable[['TornadoConnection', Exception], None]
+_OnOpenCallback = Callable[['TornadoConnection'], None]
+_OnOpenErrorCallback = Callable[['TornadoConnection', Union[str, Exception]], None]
+
+
+class TornadoConnection(base_connection.BaseConnection[tornado.ioloop.IOLoop]):
+
+    def __init__(
+        self,
+        parameters: Optional[connection.Parameters],
+        on_open_callback: Optional[_OnOpenCallback],
+        on_open_error_callback: Optional[_OnOpenErrorCallback],
+        on_close_callback: Optional[_OnCloseCallback],
+        custom_ioloop: Optional[
+            Union[
+                tornado.ioloop.IOLoop,
+                nbio_interface.AbstractIOServices,
+            ],
+        ] = ...,
+        internal_connection_workflow: bool = ...,
+    ) -> None: ...
+
     @classmethod
-    def create_connection(cls, connection_configs: Any, on_done: Any, custom_ioloop: Optional[Any] = ..., workflow: Optional[Any] = ...): ...
+    def create_connection(
+        cls,
+        connection_configs: Sequence[connection.Parameters],
+        on_done: Callable[
+            [
+                Union[
+                    connection.Connection,
+                    connection_workflow.AMQPConnectionWorkflowFailed,
+                    connection_workflow.AMQPConnectionWorkflowAborted,
+                ],
+            ],
+            None
+        ],
+        custom_ioloop: Optional[tornado.ioloop.IOLoop] = ...,
+        workflow: Optional[connection_workflow.AbstractAMQPConnectionWorkflow] = ...,
+    ) -> connection_workflow.AbstractAMQPConnectionWorkflow: ...

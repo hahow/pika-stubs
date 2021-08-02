a9dd0fb9671036df4c46476dc97272b25eaab002
Anders Kaseorg <andersk@mit.edu>
2021-08-02 16:28:46 -0700

    blocking_connection: Fix consume() return type
    Fixes #3.
    
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

--- pika-stubs/adapters/blocking_connection.pyi
+++ pika-stubs/adapters/blocking_connection.pyi
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import types
-from typing import Any, Callable, List, Mapping, Optional, Tuple, Type
+from typing import Any, Callable, Iterator, List, Mapping, Optional, Tuple, Type
 
 from .. import channel, connection, frame, spec
 
@@ -147,7 +147,13 @@ class BlockingChannel:
         exclusive: bool = ...,
         arguments: Optional[Mapping[str, Any]] = ...,
         inactivity_timeout: Optional[float] = ...,
-    ) -> None: ...
+    ) -> Iterator[
+        Tuple[
+            Optional[spec.Basic.Deliver],
+            Optional[spec.BasicProperties],
+            Optional[bytes],
+        ]
+    ]: ...
 
     def get_waiting_message_count(self) -> int: ...
 
